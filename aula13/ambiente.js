1cb5d404e2bab9659259de96fec0e051edec0580
Breno Cavalcanti <131911164+souzabs@users.noreply.github.com>
2023-05-23 21:31:19 -0300

    aula 13 while e do

---
+++ aula13/ambiente.js
@@ -0,0 +1,22 @@
+var c = 1 
+
+while(c <= 8) {
+    console.log(`Passo ${c}`) // Estrutura padrão
+    c++
+}
+
+do {
+    console.log(`Passo ${c}`) //Estrutura reversa
+    c++
+} while(c <= 8)
+
+
+
+/* console.log('Tudo bem?')
+console.log('Passo 1')
+console.log('Passo 2')
+console.log('Passo 2')
+console.log('Passo 4')
+console.log('Passo 5')
+console.log('Passo 6')
+console.log('Passo 7') */
