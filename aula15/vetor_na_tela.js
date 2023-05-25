ca8ba2eef203039ba059e467fae82b40ba66aad9
Breno Cavalcanti <131911164+souzabs@users.noreply.github.com>
2023-05-24 23:28:39 -0300

    final att e indexOf

--- aula15/vetor_na_tela.js
+++ aula15/vetor_na_tela.js
@@ -4,8 +4,14 @@ var valores = [8, 1, 7, 4, 2, 9]
 console.log(valores[0])
 console.log(valores[0])            Método funcional, porém pouco usual e inteligente.
 console.log(valores[0])
-console.log(valores[0]) */
+console.log(valores[0]) 
+
 
 for(var pos=0; pos < valores.length; pos++){
-    console.log(`A posição ${pos} tem o valor ${valores[pos]}`)
+    console.log(`A posição ${pos} tem o valor ${valores[pos]}`)      Método padrão
+}
+*/
+
+for(let pos in valores){
+    console.log(`A posição ${pos} tem o valor ${valores[pos]}`)      //Método proeficiente
 }
