66de1d79adf9937ae951f6f2ce7e3061f6233b26
Breno Cavalcanti <131911164+souzabs@users.noreply.github.com>
2023-05-25 22:23:53 -0300

    att aula15 e 16(notes)

--- aula15/vetor_na_tela.js
+++ aula15/vetor_na_tela.js
@@ -1,14 +1,14 @@
 var valores = [8, 1, 7, 4, 2, 9]
 
-/* console.log(valores[0])
+ /*console.log(valores[0])
 console.log(valores[0])
-console.log(valores[0])            Método funcional, porém pouco usual e inteligente.
+console.log(valores[0])            //Método funcional, porém pouco usual e inteligente.
 console.log(valores[0])
 console.log(valores[0]) 
 
 
 for(var pos=0; pos < valores.length; pos++){
-    console.log(`A posição ${pos} tem o valor ${valores[pos]}`)      Método padrão
+    console.log(`A posição ${pos} tem o valor ${valores[pos]}`)      //Método padrão
 }
 */
 
