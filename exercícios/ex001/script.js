91be9b5ca391d58d86dc2fa41bafa66bd937fcdf
Breno Cavalcanti <131911164+souzabs@users.noreply.github.com>
2023-05-27 18:44:40 -0300

    finalização ex001

--- exercícios/ex001/script.js
+++ exercícios/ex001/script.js
@@ -1,19 +1,24 @@
-function carregar() {
-var msg = document.getElementById('msg')
-var img = document.getElementById('imagem')
-var data = new Date()
-var hora = data.getHours()
-msg.innerHTML = `Agora são ${hora} horas.`
+function carregar(){
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours() 
+
+
 if(hora >= 0 && hora < 12){
-    //BOM DIA!
-    img.src = 'manha.jpeg'
+    img.src = 'imagens/foto-manha.png'
+    document.body.style.background = '#FFC26C'
+    msg.innerHTML = `Agora são ${hora} horas. <br>Bom dia!`
+
+}else if(hora >= 12 && hora < 18){
+    img.src = 'imagens/foto-tarde.png'
+    document.body.style.background = '#C0D8D8'
+    msg.innerHTML = `Agora são ${hora} horas. <br>Boa tarde!`
+
+}else{
+    img.src = 'imagens/foto-noite.png'
+    document.body.style.background = '#203F54'
+    msg.innerHTML = `Agora são ${hora} horas. <br>Boa noite!`
 }
-else if(hora >= 12 && hora < 18){
-    //BOA TARDE
-    img.src = 'tarde.png'
-}
-else{
-    //BOA NOITE!
-    img.src = 'noite.jpeg'
-}
+
 }
