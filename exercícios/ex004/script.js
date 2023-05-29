caf8491cf15505dc632b2c4cf065ecd9edaf3180
Breno Cavalcanti <131911164+souzabs@users.noreply.github.com>
2023-05-28 23:22:56 -0300

    script

---
+++ exercícios/ex004/script.js
@@ -0,0 +1,10 @@
+function tabuada(){
+    let num = document.getElementById('txtn')
+    let tab = document.getElementById('seltab')
+    if(num.value.lenght == 0){
+        alert('Por favor, digite um número!')
+    }else{
+        let n = Number(num.value)
+    }
+    let n = Number(num.value)
+}
