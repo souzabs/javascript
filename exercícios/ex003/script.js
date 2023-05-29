bfa994399ec200f093621f6583a9d74c4bc3fc8d
Breno Cavalcanti <131911164+souzabs@users.noreply.github.com>
2023-05-28 22:35:49 -0300

    var e for

--- exercícios/ex003/script.js
+++ exercícios/ex003/script.js
@@ -6,13 +6,17 @@ function contar(){
     if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
         alert('[ERRO] Faltam dados!')
     } else{
-     res.innerHTML = 'Contando'
+     res.innerHTML = 'Contando: '
      let i = Number(ini.value)
      let f = Number(fim.value) 
      let p = Number(passo.value)
-
-     for(let c = i; c <= f; c += p){
-        res.innerHTML += `${c} \u{1F603}`
+    
+     if(i < f){
+        for(let c = i; c <= f; c += p){
+        res.innerHTML += `${c} \u{1F449}`
+       }
+     res.innerHTML += `\u{1F3C1}`
      }
+     
     }
 } 
