b106940be2c1486968065ae57271e4d341381465
Breno Cavalcanti <131911164+souzabs@users.noreply.github.com>
2023-05-28 22:46:21 -0300

    correções e finalização

--- exercícios/ex003/script.js
+++ exercícios/ex003/script.js
@@ -4,19 +4,29 @@ function contar(){
     let passo = document.getElementById('txtp')
 
     if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
-        alert('[ERRO] Faltam dados!')
+        res.innerHTML = 'Impossível contar'
     } else{
-     res.innerHTML = 'Contando: '
+     res.innerHTML = 'Contando: </br>' 
      let i = Number(ini.value)
      let f = Number(fim.value) 
      let p = Number(passo.value)
-    
+     if(p <= 0){
+        alert('Passo inválido! Considerando PASSO 1')
+        p = 1
+     }
      if(i < f){
+        //Contagem progressiva
         for(let c = i; c <= f; c += p){
         res.innerHTML += `${c} \u{1F449}`
        }
-     res.innerHTML += `\u{1F3C1}`
-     }
      
+     }
+     else {
+        //Contagem regressiva
+        for(let c = 1; c <= f; c += p){
+            res.innerHTML += `${c} \u{1F449}`
+        }
+     }
+     res.innerHTML += `\u{1F3C1}`
     }
 } 
