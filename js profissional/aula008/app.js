c50050e059dc78e845d2df4067246b41c259f912
Breno Cavalcanti <131911164+souzabs@users.noreply.github.com>
2023-05-31 19:38:18 -0300

    ou simples

--- js profissional/aula008/app.js
+++ js profissional/aula008/app.js
@@ -15,3 +15,19 @@ let n2 = 11
 
 let res = n1 & n2
 console.log(res)
+
+
+/* Operador |:
+10 em binary code -> 00001010 
+11 em binary code -> 00001011
+Retorno  --->        00001011 
+Isso ocorre porque diferentemente do primeiro caso, onde era
+necessário a equivalencia entre ambos os códigos binários, 
+nesse caso especificamente, basta ter o número 1 em uma
+das colunas que ele já vai retornar o valor, como mostrado acima.*/
+
+let n3 = 10 
+let n4 = 11
+
+let res1 = n1 | n2
+console.log(res)
