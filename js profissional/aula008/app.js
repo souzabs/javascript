efa55ec5e50c91c6d9c2997e09e76ce43b6f6de2
Breno Cavalcanti <131911164+souzabs@users.noreply.github.com>
2023-05-31 19:57:17 -0300

    operador ^

--- js profissional/aula008/app.js
+++ js profissional/aula008/app.js
@@ -17,6 +17,7 @@ let res = n1 & n2
 console.log(res)
 
 
+
 /* Operador |:
 10 em binary code -> 00001010 
 11 em binary code -> 00001011
@@ -30,4 +31,21 @@ let n3 = 10
 let n4 = 11
 
 let res1 = n1 | n2
+console.log(res)
+
+
+
+/* Operador ^:
+10 em binary code -> 00001010 
+11 em binary code -> 00001011
+Retorno  --->        00000001 
+Nesse caso específico, ocorre da equivalência ser anulada sempre a 0
+independente se vai ser 0 a 0 ou 1 a 1. Ele só vai reportar 1 na 
+comparação binária, quando só tiver no seguinte caso: 0 a 1 ou 1 a 1,
+como mostrado no exemplo acima. */
+
+let n5 = 10 
+let n6 = 11
+
+let res2 = n1 ^ n2
 console.log(res)
