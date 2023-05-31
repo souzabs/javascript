4ce76a81be250b46f1a480c265eba9856adb3347
Breno Cavalcanti <131911164+souzabs@users.noreply.github.com>
2023-05-30 21:34:22 -0300

    let example

--- js profissional/aula003/app.js
+++ js profissional/aula003/app.js
@@ -1,1 +1,19 @@
 "strict use"
+
+//Exemplo var
+/* */
+
+
+//Exemplo let
+/* No caso abaixo, o let restringe a utilização da variável, apenas podendo "puxar" a que se encontra dentro
+do escopo/bloco IF. Qualquer uma outra tentativa de referenciar a variável não irá funcionar*/
+function test(){
+    if(true){
+        let nome = "Bruno"
+        console.log('Dentro do IF do teste' + nome)
+    } 
+    console.log('Dentro da function test' + nome)
+}
+console.log('Fora da function' + nome)
+
+
