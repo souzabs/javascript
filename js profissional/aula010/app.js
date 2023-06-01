35cb2e5bc5a729ba9ce20fdf797d03d353217daa
Breno Cavalcanti <131911164+souzabs@users.noreply.github.com>
2023-06-01 20:42:44 -0300

    correc

--- js profissional/aula010/app.js
+++ js profissional/aula010/app.js
@@ -1,16 +1,6 @@
 "strict use"
 
 //Operador ternário
-/* Ele é frequentemente utilizado como uma forma de substituição do 
-if/else. Pois o operador ternário realiza a operação na primeira 
-parte, se o resultado da operação for verdadeiro ele retorna o 
-valor inserido na segunda parte(que no caso é "Par"), caso ele
-seja falso, ele irá retornar a terceira parte da operação("Impar"). */ 
-
-let num = 10 
-res = (!(num%2) ? "Par" : "Impar")
-console.log(res)
-
 
 
 //Metodo padrão com if/else
@@ -21,4 +11,18 @@ if(res1 == 0){
     console.log('Par')
 } else{
     console.log('Impar')
-}
+} 
+
+
+
+//Metodo com operador ternário
+
+let num = 10
+let res = (!(num%2) ? "Par" : "Impar")
+console.log(res)//Par
+
+/* Ele é frequentemente utilizado como uma forma de substituição do 
+if/else. Pois o operador ternário realiza a operação na primeira 
+parte, se o resultado da operação for verdadeiro ele retorna o 
+valor inserido na segunda parte(que no caso é "Par"), caso ele
+seja falso, ele irá retornar a terceira parte da operação("Impar"). */ 
