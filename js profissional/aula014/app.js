f17528a466e6ec6fb0eb3e2ee748f1dc0149fed1
Breno Cavalcanti <131911164+souzabs@users.noreply.github.com>
2023-06-01 22:46:19 -0300

    aula 014 switch

---
+++ js profissional/aula014/app.js
@@ -0,0 +1,28 @@
+"strict use"
+
+let colocacao = 1
+
+switch(colocacao){
+    case 1:
+        console.log('Primeiro lugar!')
+    break
+
+     case 2:
+        console.log('Segundo lugar!')
+    break
+
+     case 3:
+        console.log('Terceiro lugar!')
+    break
+
+    default:
+        console.log("Não subiu ao pódio")
+}
+
+/* Switch Case
+O funcionamento da estrutura switch é bastante simples. Inicialmente,
+o valor da variável passada no switch é comparado com os valores fornecidos
+em cada case. Se um desses valores for igual ao valor da variável, o bloco 
+de código do case em questão será executado. Ao fim de casa switch case, é
+necessário utilizar o break para pular para a próxima case. Ao fim dos
+switchs cases, utilizar o deafult para um valor padrão.*/
