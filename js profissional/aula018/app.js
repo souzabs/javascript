4f99800c0d2d889862f8fe85033e511d28f73f58
Breno Cavalcanti <131911164+souzabs@users.noreply.github.com>
2023-06-04 19:04:49 -0300

    ex018

--- js profissional/aula018/app.js
+++ js profissional/aula018/app.js
@@ -5,4 +5,20 @@
 Existe uma diferença clara no uso entre eles. Primeiro que no while, 
 o primeiro a se fazer é o teste lógico e depois os comandos são
 executados. No Do While, primeiro se executa os comandos e depois 
-o teste lógico é executado.*/
+o teste lógico é executado. */
+
+ let n = 10
+ /* while(n < 10){
+    console.log('É menor que 10!')
+    n++                                   ----> Método While
+}
+console.log('Fim do programa!') */
+
+do{
+    console.log('É menor que 10!')
+    n++                                 // ----> Método Do While
+}while(n < 10){
+    console.log('Fim do programa!')
+}
+
+/* O loop foi iterado ao menos uma vez no caso do while*/
