be4f46e2cf8c3c5c35e9a6b6c46e97f5189b0cbb
Breno Cavalcanti <131911164+souzabs@users.noreply.github.com>
2023-06-04 18:58:04 -0300

    while e do while

---
+++ js profissional/aula018/app.js
@@ -0,0 +1,8 @@
+"strict use"
+
+/* While e Do While
+
+Existe uma diferença clara no uso entre eles. Primeiro que no while, 
+o primeiro a se fazer é o teste lógico e depois os comandos são
+executados. No Do While, primeiro se executa os comandos e depois 
+o teste lógico é executado.*/
