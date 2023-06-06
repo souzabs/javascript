61240aca5fbc9161d62a58d0c872d485b42a0619
Breno Cavalcanti <131911164+souzabs@users.noreply.github.com>
2023-06-06 12:47:38 -0300

    valor padrão

--- js profissional/aula022/app.js
+++ js profissional/aula022/app.js
@@ -6,7 +6,8 @@ function soma(p1){
  console.log(p1)
 }
 
-soma("Bruno")
+soma("Bruno") // ---> Chamada e declaração do parâmetro
+
 
 
 /* Soma dos parâmetros dem ser
@@ -16,6 +17,20 @@ function soma(n1, n2){
     console.log(n1 * n2)
    }
    
-   soma(10, 5)
+soma(10, 5) // ---> Chamada e declaração do parâmetro
    
+   
+
+/*Nesse caso específico é possível perceber que 
+lá no final do código, na parte da declaração, apenas 
+o primeiro valor(n1) foi declarado. Normalmente o retorno 
+seria de NaN(Not a Number), pois apenas o primeiro valor 
+foi declaro e o segundo ficou como indefinido. Porém,
+é possível assumir um valor padrão para n1 e n2 dentro da função,
+que no caso específico é zero para ambos. */
+function soma(n1=0, n2=0){
+    console.log(n1 * n2)
+   }
+   
+soma(10) // ---> Chamada e declaração do parâmetro
    
