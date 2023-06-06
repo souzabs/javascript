009238e9439c3980865e593b939deed5cd7967ef
Breno Cavalcanti <131911164+souzabs@users.noreply.github.com>
2023-06-06 12:40:26 -0300

    att  paramet

--- js profissional/aula022/app.js
+++ js profissional/aula022/app.js
@@ -1,3 +1,21 @@
 "strict use"
 
+//Funções parametrizadas
 
+function soma(p1){
+ console.log(p1)
+}
+
+soma("Bruno")
+
+
+/* Soma dos parâmetros dem ser
+declarados na mesma linha, como 
+está sendo mostrado abaixo.*/
+function soma(n1, n2){
+    console.log(n1 * n2)
+   }
+   
+   soma(10, 5)
+   
+   
