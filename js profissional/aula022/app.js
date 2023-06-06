4ed61a451f1bc774e82bbf12859eee7f90c408d1
Breno Cavalcanti <131911164+souzabs@users.noreply.github.com>
2023-06-06 12:54:59 -0300

    changes

--- js profissional/aula022/app.js
+++ js profissional/aula022/app.js
@@ -27,8 +27,20 @@ o primeiro valor(n1) foi declarado. Normalmente o retorno
 seria de NaN(Not a Number), pois apenas o primeiro valor 
 foi declaro e o segundo ficou como indefinido. Porém,
 é possível assumir um valor padrão para n1 e n2 dentro da função,
-que no caso específico é zero para ambos. */
-function soma(n1=0, n2=0){
+que no caso específico é um para ambos. */
+function soma(n1 = 1, n2 = 1){
+    console.log(n1 * n2)
+   }
+   
+soma(10) // ---> Chamada e declaração do parâmetro
+   
+
+
+/* Também é possível ter o mesmo resultado acima
+definindo uma váriavel, como no exemplo abaixo e em seguida,
+declarar a váriavel dentro de n1 e n2(outras váriaveis) */
+const VALOR_PADRAO = 1
+function soma(n1 = VALOR_PADRAO, n2 = VALOR_PADRAO){
     console.log(n1 * n2)
    }
    
