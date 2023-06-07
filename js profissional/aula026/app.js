11096e642b4ed877552335ae3eedf310ff11318f
Breno Cavalcanti <131911164+souzabs@users.noreply.github.com>
2023-06-06 21:54:29 -0300

    aninhamento

---
+++ js profissional/aula026/app.js
@@ -0,0 +1,16 @@
+"strict use"
+
+//Aninhamento
+
+const soma = (...valores) => {
+    const somar = val => {
+        let res = 0
+        for(v of val){
+            res += v
+        }
+        return res
+    }
+    return somar(valores)
+}
+
+console.log(soma(10, 5 , 15))
