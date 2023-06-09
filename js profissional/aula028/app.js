1edcad7b1229cb9850384ea946139d5ed778cd22
Breno Cavalcanti <131911164+souzabs@users.noreply.github.com>
2023-06-08 22:19:17 -0300

    final

--- js profissional/aula028/app.js
+++ js profissional/aula028/app.js
@@ -7,7 +7,8 @@ no objeto array para facilitar a sua manipulação. Basicamente,
 ele faz a leitura de todos os elementos do array, executa uma 
 função callback para cada um e devolve como retorno um novo array.*/
 
-const  cursos = ['HTML', 'CSS', 'Javascript', 'PHP', 'React']
+/*const  cursos = ['HTML', 'CSS', 'Javascript', 'PHP', 'React']
 cursos.map((el, i) => {
     console.log("Curso: " + el + " - Posição do curso: " + i)
-})
+}) */
+
