240dff78579f419540bb90efaa037db5f0ed1e0f
Breno Cavalcanti <131911164+souzabs@users.noreply.github.com>
2023-06-08 22:09:57 -0300

    att

--- js profissional/aula028/app.js
+++ js profissional/aula028/app.js
@@ -1,1 +1,13 @@
 "strict use"
+
+//Método Map
+
+/* O método map() pertence ao conjunto de funções disponíveis 
+no objeto array para facilitar a sua manipulação. Basicamente, 
+ele faz a leitura de todos os elementos do array, executa uma 
+função callback para cada um e devolve como retorno um novo array.*/
+
+const  cursos = ['HTML', 'CSS', 'Javascript', 'PHP', 'React']
+cursos.map((el, i) => {
+    console.log("Curso: " + el + " - Posição do curso: " + i)
+})
