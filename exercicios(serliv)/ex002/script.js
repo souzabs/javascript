0b4e05bdac2d1a70aaee35fbf7e95d6526229565
Breno Cavalcanti <131911164+souzabs@users.noreply.github.com>
2023-06-09 21:01:33 -0300

    att pastas

--- exercicios(serliv)/ex002/script.js
+++
@@ -1,9 +0,0 @@
-function calcule(){
-var n1 = prompt('Digite  um número: ')
-n1 = parseInt(n1)
-
-var n2 = prompt('Digite outro número:')
-n2 = parseInt(n2)
-
-alert(`A subtração é igual a ${n1 - n2}.`)
-}
