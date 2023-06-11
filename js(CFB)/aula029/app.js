aa69a92fa592114b57c208658b5caa61657fe456
Breno Cavalcanti <131911164+souzabs@users.noreply.github.com>
2023-06-11 17:42:35 -0300

    this

--- js(CFB)/aula029/app.js
+++ js(CFB)/aula029/app.js
@@ -1,1 +1,27 @@
 "strict use"
+
+/* function aluno(nome, nota){
+ this.nome = nome
+ this.nota = nota
+
+ console.log(nome)
+ console.log(nota)
+}
+
+aluno("Bruno", 10) */ 
+
+
+function aluno(nome, nota){
+    this.nome = nome
+    this.nota = nota
+   
+    this.dados_arrow = function(){
+        setTimeout(() => {
+          console.log(this.nome)
+          console.log(this.nota)
+        }, 2000)
+    }
+ }
+   
+ const al1 = new aluno("Bruno", 10)
+  al1.dados_arrow()
