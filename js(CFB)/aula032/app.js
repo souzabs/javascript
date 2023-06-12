502cce0f124fff582f0f066b4283ab5cf39b2e4b
Breno Cavalcanti <131911164+souzabs@users.noreply.github.com>
2023-06-11 22:04:51 -0300

    att

--- js(CFB)/aula032/app.js
+++ js(CFB)/aula032/app.js
@@ -1,1 +1,30 @@
 "strict use"
+
+/* const cursosTodos = [...document.getElementsByClassName("curso")] 
+
+ --> Nesse caso, utiliza-se do spread (...) para transformar o HTMLCollections em um array
+
+e assim poder trabalhar com suas características. 
+
+console.log(cursosTodos) */
+
+
+const cursosTodos = [...document.getElementsByClassName("curso")] 
+const cursosC1 = [...document.getElementsByClassName("c1")] 
+const cursosC2 = [...document.getElementsByClassName("c2")] 
+
+console.log(cursosTodos)
+console.log(cursosC1)
+console.log(cursosC2)
+
+/* cursosTodos.map((el) => {
+  el.classList.add("destaque")
+}) */
+
+ cursosC1.map((el) => {
+    el.classList.add("destaque")
+  }) 
+
+  /* cursosC2.map((el) => {
+    el.classList.add("destaque")
+  }) */ 
