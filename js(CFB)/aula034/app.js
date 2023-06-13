89f9485936b0052c45dfde28d75495ea02df4821
Breno Cavalcanti <131911164+souzabs@users.noreply.github.com>
2023-06-13 20:22:13 -0300

    att

--- js(CFB)/aula034/app.js
+++ js(CFB)/aula034/app.js
@@ -1,25 +1,9 @@
 "strict use"
 
-const divTodas = [...document.getElementsByTagName("div")] 
-const cursosTodos = [...document.getElementsByClassName("curso")] 
-const cursosC1 = [...document.getElementsByClassName("c1")] 
-const cursosC2 = [...document.getElementsByClassName("c2")] 
-const cursosEspecial = [...document.getElementById("c1")] 
+const c1 = document.getElementById("c1")
+const msg = () => {
+  alert("clicou!")
+}
 
-const query_divTodas = [...document.querySelectorAll("div")]
-const query_cursosTodos = [...document.querySelectorAll(".curso")] 
-const query_cursosC1 = [...document.querySelectorAll(".c1")]
-const query_cursosC2 = [...document.querySelectorAll(".c2")]
-
-console.log(query_divTodas)
-
-/* console.log(divTodas)
-console.log(cursosTodos)
-console.log(cursosC1)
-console.log(cursosC2)
-
- cursosC1.map((el) => {
-    el.classList.add("destaque")
-  })  */
-
+c1.addEventListener("click", msg)
   
